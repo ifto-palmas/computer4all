94c1aebe686d8b2f970d3267b70d4fca63da5682
Manoel Campos <manoelcampos@gmail.com>
2022-09-21 15:12:58 -0300

    Adds initial server code

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get('/', (_, res) => res.sendFile('/index.html'));
+
+//Porta usada internamente para evitar rodar o node como root
+const internalPort = 8080;
+
+//Porta usada externamente, quando o app é executado no Docker
+const externalPort = process.env.PORT || internalPort;
+const host = process.env.SERVER || "localhost"
+const address = externalPort == 80 ? host : `${host}:${externalPort}`;
+
+app.listen(internalPort, () => {
+    console.log('Servidor iniciado. Abra o navegador em http://' + address);
+});
