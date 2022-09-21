1de4405bebd7fb3960f8e7df92d81890dcf3e695
Manoel Campos <manoelcampos@gmail.com>
2022-09-21 15:49:51 -0300

    Enable cors

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (_, res) => res.sendFile('/index.html'))
 
