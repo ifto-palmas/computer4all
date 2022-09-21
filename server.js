57f758c45241b4bfd8a99340d753e454caf25c1b
Manoel Campos <manoelcampos@gmail.com>
2022-09-21 15:21:35 -0300

    Server code refactoring

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ app.use(express.json());
 
 app.get('/', (_, res) => res.sendFile('/index.html'));
 
-//Porta usada internamente para evitar rodar o node como root
-const internalPort = 8080;
+const DEFAULT_PORT = 8080;
 
-//Porta usada externamente, quando o app é executado no Docker
-const externalPort = process.env.PORT || internalPort;
-const host = process.env.SERVER || "localhost"
-const address = externalPort == 80 ? host : `${host}:${externalPort}`;
+//Porta usada externamente, quando o app é executado em um servidor como heroku.com
+const PORT = process.env.PORT || DEFAULT_PORT;
+const HOST = process.env.SERVER || "localhost"
+const ADDRESS = PORT == 80 ? HOST : `${HOST}:${PORT}`;
 
-app.listen(internalPort, () => {
-    console.log(`\n\nServidor iniciado. Abra o navegador em http://${address}\n`);
+app.listen(PORT, () => {
+    console.log(`\n\nServidor iniciado. Abra o navegador em http://${ADDRESS}\n`);
 });
