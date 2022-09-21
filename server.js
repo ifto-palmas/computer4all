022a8c3dbdc57ac9f59b493557da7cc462f72d6c
Manoel Campos <manoelcampos@gmail.com>
2022-09-21 15:32:22 -0300

    Server code format

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use(express.static("public"));
-app.use(express.json());
+app.use(express.static("public"))
+app.use(express.json())
 
-app.get('/', (_, res) => res.sendFile('/index.html'));
+app.get('/', (_, res) => res.sendFile('/index.html'))
 
-const DEFAULT_PORT = 8080;
+const DEFAULT_PORT = 8080
 
 //Porta usada externamente, quando o app é executado em um servidor como heroku.com
-const PORT = process.env.PORT || DEFAULT_PORT;
+const PORT = process.env.PORT || DEFAULT_PORT
 const HOST = process.env.SERVER || "localhost"
-const ADDRESS = PORT == 80 ? HOST : `${HOST}:${PORT}`;
+const ADDRESS = PORT == 80 ? HOST : `${HOST}:${PORT}`
 
 app.listen(PORT, () => {
-    console.log(`\n\nServidor iniciado. Abra o navegador em http://${ADDRESS}\n`);
-});
+    console.log(`\n\nServidor iniciado. Abra o navegador em http://${ADDRESS}\n`)
+})
