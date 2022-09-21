2d016f4655da56ab71ce3c30cbdcf9590590b73a
Manoel Campos <manoelcampos@gmail.com>
2022-09-21 15:48:50 -0300

    Changes quotes in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 
-app.use(express.static("public"))
+app.use(express.static('public'))
 app.use(express.json())
 
 app.get('/', (_, res) => res.sendFile('/index.html'))
@@ -11,7 +11,7 @@ const DEFAULT_PORT = 8080
 
 //Porta usada externamente, quando o app é executado em um servidor como heroku.com
 const PORT = process.env.PORT || DEFAULT_PORT
-const HOST = process.env.SERVER || "localhost"
+const HOST = process.env.SERVER || 'localhost'
 const ADDRESS = PORT == 80 ? HOST : `${HOST}:${PORT}`
 
 app.listen(PORT, () => {
