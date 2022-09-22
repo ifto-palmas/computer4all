37fca8aa680315b2b90166c4a4a4322a280303b9
Manoel Campos <manoelcampos@gmail.com>
2022-09-22 11:45:22 -0300

    Add local software installation action in software controller
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- controllers/software.js
+++ controllers/software.js
@@ -1,6 +1,9 @@
 const httperror = require('../lib/httperror')
 const yml = require('../lib/yml')
 
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
 const load = async () => await yml.parse('config/softwares.yml')
 
 const isThereAnySoftware = (response, softwares) => {
@@ -10,6 +13,32 @@ const isThereAnySoftware = (response, softwares) => {
     }
 
     return true
+}
+
+/**
+ * Instala um software localmente
+ * @param softwares objeto contendo informações do software a ser instalado via apt,
+ *        obtido do config/softwares.yml
+ */
+const localInstall = async (software) => {
+    if(!software) {
+        throw new Error("É preciso um objeto contendo informações do software a ser instalado")
+    }
+
+    console.log('Updating repositories list')
+    /*const { stdout, stderr } =*/ await exec('apt update')
+
+    console.log(`Preparing for intallation of ${software.description} (${software.aptName})`)
+    if (software.preinstall) {
+        console.log(`Installing dependencies for ${software.aptName}`)
+        /*const { stdout, stderr } =*/ exec(software.preinstall)
+    }
+
+    console.log(`Installing ${software.aptName}`)
+    const command = `apt install ${software.aptName}`
+    /*const { stdout, stderr } =*/ exec(command)
+    console.log(command)
+
 }
 
 exports.get = async (req, res) => {
@@ -34,6 +63,28 @@ exports.all = async (req, res) => {
         const softwares = await load()
         if(isThereAnySoftware(res, softwares))
             res.status(200).send(softwares)
+    } catch (e) {
+        httperror.server(res, e)
+    }
+}
+
+exports.install = async (req, res) => {
+    try {
+        const softwares = await load()
+
+        if(!isThereAnySoftware(res, softwares))
+            return
+
+        const software = softwares.find(s => s.aptName.toLowerCase() === req.params.aptName.toLowerCase())
+
+        if (!software) {
+            const notFoundMsg = `Não existe configuração para instalação do ${req.params.aptName}. Entre em contato com o administrador do sistema.`
+            httperror.custom(res, notFoundMsg, 404)
+            return
+        }
+
+        await localInstall(software)
+        res.status(201).send()
     } catch (e) {
         httperror.server(res, e)
     }
